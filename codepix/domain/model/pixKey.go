c2424172d3b4c300c6891edfaa2de7a63311b0fa
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-10-23 11:39:37 -0300

    feat: create pix key repository interface

--- codepix/domain/model/pixKey.go
+++ codepix/domain/model/pixKey.go
@@ -1,10 +1,19 @@
 package model
 
 import (
+	"errors"
 	"time"
 	uuid "github.com/satori/go.uuid"
 	"github.com/asaskevich/govalidator"
 )
+
+type PixKeyRepositoryInterface interface {
+	RegisterKey(pixKey *PixKey) (*PixKey, error)
+	FindKeyByKind(key string, kind string) (*PixKey, error)
+	AddBank(bank *Bank) error
+	AddAccount(account *Account) error
+	FindAccount(id string) (*Account, error)
+}
 
 type PixKey struct {
 	Base      `valid:"required"`

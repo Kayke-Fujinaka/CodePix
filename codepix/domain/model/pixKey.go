d92c8852615cbedc1dde4d89f10c31faf83c4934
Kayke-Fujinaka <kaykealvesfujinaka@gmail.com>
2023-10-23 11:31:39 -0300

    feat: create pix key model

---
+++ codepix/domain/model/pixKey.go
@@ -0,0 +1,45 @@
+package model
+
+import (
+	"time"
+	uuid "github.com/satori/go.uuid"
+	"github.com/asaskevich/govalidator"
+)
+
+type PixKey struct {
+	Base      `valid:"required"`
+	Kind      string   `json:"kind" valid:"notnull"`
+	Key       string   `json:"key" valid:"notnull"`
+	AccountID string   `json:"account_id" valid:"-"`
+	Account   *Account `valid:"-"`
+	Status    string   `json:"status" valid:"notnull"`
+}
+
+func (pixKey *PixKey) isValid() error {
+	_, err := govalidator.ValidateStruct(pixKey)
+
+	if err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func NewPixKey(kind string, account *Account, key string) (*PixKey, error) {
+	pixKey := PixKey{
+		Kind:      kind,
+		Key:       key,
+		Account:   account,
+		Status:    "active",
+	}
+
+	pixKey.ID = uuid.newV4().String()
+	pixKey.CreatedAt = time.Now()
+
+	err := pixKey.isValid()
+	if err != nil {
+		return nil, err
+	}
+
+	return &pixKey, nil
+}
